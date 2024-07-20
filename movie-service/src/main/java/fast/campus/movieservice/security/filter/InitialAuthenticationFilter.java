f9b0ac17b9a530c6255f6e5f25f7311e48fbad97
kdohyeon <kdohyeon@gmail.com>
2024-07-20 22:42:23 +0900

    readme 업데이트

--- movie-service/src/main/java/fast/campus/movieservice/security/filter/InitialAuthenticationFilter.java
+++ movie-service/src/main/java/fast/campus/movieservice/security/filter/InitialAuthenticationFilter.java
@@ -13,7 +13,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
